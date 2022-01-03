534482d6992d35c4c021133d05f3178ff38699d3
naiara.pedrozo <naiara.pedrozo@faci.ly>
2022-01-03 14:23:55 -0300

    feature/cryptography

--- cryptography/cryptography_test.go
+++ cryptography/cryptography_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// key required 16 digits, nonce required 12 digits
+// key required 16 digits, nonce required 12 digits.
 func TestEncryptDecrypt(t *testing.T) {
 	telefone := "999999999"
 	crip := NewCryptography([]byte("XXXXXfacilyXXXXX"), []byte("XXXfacilyXXX"))
