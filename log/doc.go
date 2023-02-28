069c96d65adf8d25bc6fe953588bdd30575e95e5
David Valentim Dias <david.dias@faci.ly>
2023-02-28 16:05:24 -0300

    lint: include log/doc.go

---
+++ log/doc.go
@@ -0,0 +1,4 @@
+// Package log is a interface for anyone who do not want to implement their own
+// interface to popular logs. log has integration with telemetry trace for
+// continuous liking between web request session at log.
+package log
