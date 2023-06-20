a27d0ff26ca29a4a9f5de99b527c90163ad646b6
David Valentim Dias <david.dias@faci.ly>
2023-06-20 17:27:06 -0300

    feat(log): With allow creating Logger with fields

--- log/zap_test.go
+++ log/zap_test.go
@@ -6,7 +6,10 @@ import (
 	"testing"
 
 	"github.com/facily-tech/go-core/telemetry"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
 )
 
 func Test_fieldsToZap(t *testing.T) {
@@ -46,6 +49,48 @@ func Test_fieldsToZap(t *testing.T) {
 			if got := fieldsToZap(tt.args.ctx, tt.args.tracer, tt.args.fs); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("fieldsToZap() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestZap_With(t *testing.T) {
+	buff := &zaptest.Buffer{}
+	type fields struct {
+		logger *zap.Logger
+		tracer telemetry.Tracer
+	}
+	type args struct {
+		ctx    context.Context
+		fields []Field
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   Logger
+	}{
+		{
+			name: "included field must be present?",
+			fields: fields{
+				logger: zap.New(zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewDevelopmentConfig().EncoderConfig), buff, zap.DebugLevel)),
+				tracer: nil,
+			},
+			args: args{
+				ctx:    context.Background(),
+				fields: []Field{Any("versionApp", "4.39.2")},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			z := &Zap{
+				logger: tt.fields.logger,
+				tracer: tt.fields.tracer,
+			}
+			z.With(tt.args.ctx, tt.args.fields...).Info(tt.args.ctx, "ola")
+			assert.Contains(t, buff.String(), "versionApp")
+			assert.Contains(t, buff.String(), "4.39.2")
+
 		})
 	}
 }
