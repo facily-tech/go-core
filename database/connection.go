b84a458cee8c2d95a53f18c1cfb961fac8b1887b
Gabriel Barbosa <gabriel.barbosa@faci.ly>
2023-09-13 18:45:12 -0300

    refactor module and add mysql and mongodb conn

---
+++ database/connection.go
@@ -0,0 +1,153 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	mysqldriver "github.com/go-sql-driver/mysql"
+	sqltrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/database/sql"
+	mongotrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/go.mongodb.org/mongo-driver/mongo"
+	gormtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorm.io/gorm.v1"
+
+	"github.com/facily-tech/go-core/env"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	// DBPrefix is the prefix for all environment variables related to the database.
+	DBPrefix = "DB_"
+	// PostgresDriverName is the name of the postgres driver.
+	PostgresDriverName = "pgx"
+	// MySQLDriverName is the name of the mysql driver.
+	MySQLDriverName = "mysql"
+	// Postgres is the enum for postgres database.
+	Postgres database = "postgres"
+	// MySQL is the enum for mysql database.
+	MySQL database = "mysql"
+)
+
+// Drivers is a map with Database enum and its driver name.
+var drivers = map[database]string{
+	Postgres: PostgresDriverName,
+	MySQL:    MySQLDriverName,
+}
+
+type database string
+
+type config struct {
+	DSN                  string        `env:"DSN,required"`
+	DSNTest              string        `env:"DSN_TEST"`
+	MaxOpenConn          int           `env:"MAX_OPEN,default=10"`
+	MaxIdleTime          time.Duration `env:"MAX_IDLE_DURATION,default=1m"`
+	MaxLifetime          time.Duration `env:"MAX_LIFETIME_DURATION,default=5m"`
+	TracerDatadogEnabled bool          `env:"TRACER_DATADOG_ENABLED,default=false"`
+}
+
+func InitDB(database database) (*gorm.DB, *sql.DB, error) {
+	return initDB(database, DBPrefix)
+}
+
+func InitDBWithPrefix(database database, dbPrefix string) (*gorm.DB, *sql.DB, error) {
+	return initDB(database, dbPrefix)
+}
+
+func InitMongoDB() (*mongo.Client, error) {
+	return initMongoDB(DBPrefix)
+}
+
+func InitMongoDBWithPrefix(dbPrefix string) (*mongo.Client, error) {
+	return initMongoDB(dbPrefix)
+}
+
+func loadEnv(dbPrefix string) (*config, error) {
+	var dbConfig *config
+	if err := env.LoadEnv(context.Background(), &dbConfig, dbPrefix); err != nil {
+		return nil, errors.Wrap(err, "cannot load db environment variable")
+	}
+
+	return dbConfig, nil
+}
+
+// InitMongoDB initializes a new mongo database connection.
+func initMongoDB(dbPrefix string) (*mongo.Client, error) {
+	dbConfig, err := loadEnv(dbPrefix)
+	if err != nil {
+		return nil, err
+	}
+
+	return openMongoConn(dbConfig)
+}
+
+// openMongoConn opens a new mongo database connection.
+func openMongoConn(dbConfig *config) (*mongo.Client, error) {
+	opts := options.Client()
+	opts.Monitor = mongotrace.NewMonitor()
+	opts.ApplyURI(dbConfig.DSN)
+	opts.MaxConnIdleTime = &dbConfig.MaxIdleTime
+
+	maxPoolSize := uint64(dbConfig.MaxOpenConn)
+	opts.MaxPoolSize = &maxPoolSize
+
+	return mongo.Connect(context.Background(), opts)
+}
+
+// InitDB initializes a new database connection.
+func initDB(database database, dbPrefix string) (*gorm.DB, *sql.DB, error) {
+	dbConfig, err := loadEnv(dbPrefix)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return open(database, dbConfig)
+}
+
+// open opens a new database connection.
+func open(database database, config *config) (*gorm.DB, *sql.DB, error) {
+	datasource := config.DSN
+	if len(config.DSNTest) > 0 {
+		datasource = config.DSNTest
+	}
+
+	var err error
+	var sqlDB *sql.DB
+
+	driverName := drivers[database]
+
+	if config.TracerDatadogEnabled {
+		if database == Postgres {
+			sqltrace.Register(driverName, &stdlib.Driver{})
+		} else if database == MySQL {
+			sqltrace.Register(driverName, &mysqldriver.MySQLDriver{})
+		}
+	}
+
+	sqlDB, err = sql.Open(driverName, datasource)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "cannot open database connection")
+	}
+
+	sqlDB.SetMaxOpenConns(config.MaxOpenConn)
+	sqlDB.SetConnMaxIdleTime(config.MaxIdleTime)
+	sqlDB.SetConnMaxLifetime(config.MaxLifetime)
+
+	var dialector gorm.Dialector
+	if database == Postgres {
+		dialector = postgres.New(postgres.Config{Conn: sqlDB})
+	} else if database == MySQL {
+		dialector = mysql.New(mysql.Config{Conn: sqlDB})
+	}
+
+	db, err := gormtrace.Open(dialector, &gorm.Config{})
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "cannot open a gorm connection")
+	}
+
+	return db, sqlDB, nil
+}
--- database/go.mod
+++ database/go.mod
@@ -4,9 +4,12 @@ go 1.20
 
 require (
 	github.com/facily-tech/go-core/env v0.1.0
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jackc/pgx/v5 v5.4.3
 	github.com/pkg/errors v0.9.1
+	go.mongodb.org/mongo-driver v1.7.5
 	gopkg.in/DataDog/dd-trace-go.v1 v1.54.0
+	gorm.io/driver/mysql v1.0.1
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.4
 )
@@ -23,21 +26,29 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/ebitengine/purego v0.5.0-alpha // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/outcaste-io/ristretto v0.2.1 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/secure-systems-lab/go-securesystemslib v0.7.0 // indirect
 	github.com/sethvargo/go-envconfig v0.3.5 // indirect
 	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
 	go4.org/unsafe/assume-no-moving-gc v0.0.0-20220617031537-928513b29760 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
